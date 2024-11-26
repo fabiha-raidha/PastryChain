639eddb58325b619293fa30939b5bb5b6b5e6e64
Fabiha <frkhan915@gmail.com>
2024-11-26 08:12:59 +0600

    Manpower tab (HeadChef) fxml and controller created

---
+++ src/main/java/oop/projectwork_oop/Manpower.java
@@ -0,0 +1,56 @@
+package oop.projectwork_oop;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Stack;
+
+public class Manpower {
+
+    @FXML
+    void onAttendanceViewButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    protected void onBackButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Head Chef.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+
+    }
+
+    @FXML
+    void onEvaluationButtonClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    protected void onLogOutButtonClick(ActionEvent event) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LogIn.fxml"));
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+
+    }
+
+    @FXML
+    void onRecruitmentButtonClick(ActionEvent event) {
+
+    }
+
+}
