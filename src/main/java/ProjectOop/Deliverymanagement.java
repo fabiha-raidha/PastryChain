9e39fd88ba1ae4721caac3185adf485f4ea09a24
Memo7fari <2211205@iub.edu.bd>
2024-12-10 23:15:10 +0600

    Create Deliverymanagement.java

---
+++ src/main/java/ProjectOop/Deliverymanagement.java
@@ -0,0 +1,39 @@
+package ProjectOop;
+
+public class Deliverymanagement {
+    String DriverName;
+    String OrderCode;
+
+    public String getDriverName() {
+        return DriverName;
+    }
+
+    public void setDriverName(String driverName) {
+        DriverName = driverName;
+    }
+
+    public String getOrderCode() {
+        return OrderCode;
+    }
+
+    public void setOrderCode(String orderCode) {
+        OrderCode = orderCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Deliverymanagement{" +
+                "DriverName='" + DriverName + '\'' +
+                ", OrderCode='" + OrderCode + '\'' +
+                '}';
+    }
+
+    public Deliverymanagement(String driverName, String orderCode) {
+        DriverName = driverName;
+        OrderCode = orderCode;
+        
+        
+    }
+
+   
+}
